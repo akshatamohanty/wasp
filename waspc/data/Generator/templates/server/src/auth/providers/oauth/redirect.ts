8c8d354261dd1b7d2549a89ec6bbab0744c0a6bf
Mihovil Ilakovac <mihovil@ilakovac.com>
2024-03-13 09:29:29 +0100

    Uses serverUrl from env

--- waspc/data/Generator/templates/server/src/auth/providers/oauth/redirect.ts
+++ waspc/data/Generator/templates/server/src/auth/providers/oauth/redirect.ts
@@ -2,10 +2,8 @@ import { config } from 'wasp/server'
 
 export const callbackPath = 'callback'
 
-// TODO: we need a server URL here, but we don't have it yet.
-const serverUrl = `http://localhost:3001`
 export function getRedirectUriForCallback(providerName: string) {
-  return `${serverUrl}/auth/${providerName}/${callbackPath}`;
+  return `${config.serverUrl}/auth/${providerName}/${callbackPath}`;
 }
 
 export function getRedirectUriForOneTimeCode(oneTimeCode: string) {
